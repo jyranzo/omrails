775a8e0ae307fcce550f795ae6392f0bc0bafe7d
Javier Yranzo <javieryranzo@gmail.com>
2016-11-01 13:12:48 -0300

    Creating User Profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+	get '/users/:username', to: 'users#show', as: 'user'
+
   resources :skills
   resources :tweets
   ActiveAdmin.routes(self)
   devise_for :users
+  as :user do
+  	get "signin", to: 'devise/sessions#new'
+  	delete "signout", to: 'devise/sessions#destroy'
+  	get "signup", to: 'devise/registrations#new'
+  end
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
