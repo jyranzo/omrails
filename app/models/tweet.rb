3d720e4cce327aa4aabace166c8444d1d9155cca
Javier Yranzo <javieryranzo@gmail.com>
2016-11-01 10:00:29 -0300

    Lesson 4 - Done!

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,5 +2,5 @@ class Tweet < ApplicationRecord
   belongs_to :user
 
 	validates :user, presence: true
-	validates :content, presence: true, length: { maximum: 140 }
+	validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
   validates :name, presence: true
+  
 end
