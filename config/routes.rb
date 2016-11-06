07f05872c8efd747947d4bb3afd1df1fa259ac5d
Javier Yranzo <javieryranzo@gmail.com>
2016-11-06 12:37:30 -0300

    bug fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,27 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
+  devise_for :users
+  as :user do
+    get "signin", to: 'devise/sessions#new'
+    delete "signout", to: 'devise/sessions#destroy'
+    get "signup", to: 'devise/registrations#new'
+  end
 
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   get 'feed', to: 'feed#show'
 
   resources :skills
   resources :tweets
- 
-  ActiveAdmin.routes(self)
-  devise_for :users
-  as :user do
-  	get "signin", to: 'devise/sessions#new'
-  	delete "signout", to: 'devise/sessions#destroy'
-  	get "signup", to: 'devise/registrations#new'
-  end
-  root 'pages#home'
-  get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
 
- resources :users, only: :show, param: :username do
+   resources :users, only: :show, param: :username do
     member do
       post 'follow', to: 'follows#create'
       delete 'unfollow', to: 'follows#destroy'
     end
   end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
